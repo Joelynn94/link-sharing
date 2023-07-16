f9a8aee29b8a3a6db8b7678d2ced81db1b47c482
Joseph Lynn <josephlynn94@gmail.com>
2023-07-16 11:57:25 -0400

    feat: initial mockup for link and profile routes

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,17 +1,29 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import { usePathname } from "next/navigation";
 
 export default function DashboardLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const pathname = usePathname();
+
+  const isActiveLink = (href: string) => {
+    if (pathname?.startsWith(href)) {
+      return "bg-purple-100 text-purple-500";
+    } else {
+      return "text-gray-500 hover:text-purple-500";
+    }
+  };
+
   return (
     <section>
-      <nav className="bg-white rounded-md">
-        <div className="flex flex-wrap items-center justify-between mx-auto p-4">
-          <Link href="/" className="flex items-center">
+      <nav className="bg-white rounded-md p-5 mb-4">
+        <div className="flex flex-wrap items-center justify-between grow">
+          <Link href="/" className="hidden md:flex items-center">
             <Image
               src="/logo-devlinks-large.svg"
               alt="devlinks Logo"
@@ -19,36 +31,48 @@ export default function DashboardLayout({
               height={150}
             />
           </Link>
-          <div className="flex space-x-3">
+          <Link href="/" className="flex items-center md:hidden ">
+            <Image
+              src="/logo-devlinks-small.svg"
+              alt="devlinks Logo"
+              width={40}
+              height={40}
+            />
+          </Link>
+          <div className="flex">
             <Link
               href="/dashboard/links"
-              className="flex items-center px-3 py-2 bg-purple-100 text-purple-500 rounded-md"
+              className={`flex flex-grow items-center px-6 py-2 rounded-md ${isActiveLink(
+                "/dashboard/links"
+              )}`}
             >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
+                width="22"
+                height="22"
                 viewBox="0 0 16 16"
-                className="mr-2 fill-current"
+                className="fill-current"
               >
                 <path d="M8.523 11.72a.749.749 0 0 1 0 1.063l-.371.371A3.751 3.751 0 1 1 2.847 7.85l1.507-1.506A3.75 3.75 0 0 1 9.5 6.188a.753.753 0 0 1-1 1.125 2.25 2.25 0 0 0-3.086.091L3.908 8.91a2.25 2.25 0 0 0 3.183 3.183l.37-.371a.748.748 0 0 1 1.062 0Zm4.63-8.874a3.756 3.756 0 0 0-5.305 0l-.371.37A.751.751 0 1 0 8.539 4.28l.372-.37a2.25 2.25 0 0 1 3.182 3.182l-1.507 1.507a2.25 2.25 0 0 1-3.086.09.753.753 0 0 0-1 1.125 3.75 3.75 0 0 0 5.144-.152l1.507-1.507a3.756 3.756 0 0 0 .002-5.307v-.001Z" />
               </svg>
-              <h6 className="">Links</h6>
+              <h6 className="hidden md:block ml-2">Links</h6>
             </Link>
             <Link
               href="/dashboard/profile"
-              className="flex items-center px-3 py-2 rounded-md text-gray-500 hover:text-purple-500"
+              className={`flex items-center px-6 py-2 rounded-md ${isActiveLink(
+                "/dashboard/profile"
+              )}`}
             >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                width="21"
-                height="20"
+                width="22"
+                height="22"
                 viewBox="0 0 21 20"
-                className="mr-2 fill-current"
+                className="fill-current"
               >
                 <path d="M10.5 1.563A8.437 8.437 0 1 0 18.938 10 8.447 8.447 0 0 0 10.5 1.562ZM6.716 15.357a4.688 4.688 0 0 1 7.568 0 6.54 6.54 0 0 1-7.568 0Zm1.596-5.982a2.188 2.188 0 1 1 4.376 0 2.188 2.188 0 0 1-4.376 0Zm7.344 4.683a6.523 6.523 0 0 0-2.265-1.83 4.062 4.062 0 1 0-5.782 0 6.522 6.522 0 0 0-2.265 1.83 6.562 6.562 0 1 1 10.304 0h.008Z" />
               </svg>
-              <h6 className="">Profile Details</h6>
+              <h6 className="hidden md:block ml-2">Profile Details</h6>
             </Link>
           </div>
           <div className="flex">
@@ -56,7 +80,17 @@ export default function DashboardLayout({
               href="/dashboard/preview"
               className="px-3 py-2 rounded-md border-solid border-2 border-purple-500 hover:bg-purple-100 text-purple-500"
             >
-              <h6>Preview</h6>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="22"
+                height="22"
+                fill="none"
+                viewBox="0 0 20 20"
+                className="fill-current"
+              >
+                <path d="M19.61 9.62c-.03-.064-.714-1.583-2.225-3.095-2.023-2.02-4.572-3.088-7.385-3.088-2.812 0-5.362 1.068-7.382 3.088C1.106 8.037.422 9.556.391 9.62a.944.944 0 0 0 0 .761c.029.064.713 1.583 2.226 3.095 2.021 2.02 4.57 3.086 7.383 3.086 2.813 0 5.362-1.067 7.381-3.086 1.513-1.512 2.197-3.03 2.226-3.095a.946.946 0 0 0 .003-.761Zm-3.599 2.578c-1.677 1.651-3.7 2.49-6.01 2.49-2.313 0-4.334-.839-6.01-2.491A10.185 10.185 0 0 1 2.307 10a10.192 10.192 0 0 1 1.686-2.196C5.667 6.15 7.688 5.312 10 5.312s4.333.839 6.009 2.492c.659.652 1.226 1.39 1.685 2.196a10.19 10.19 0 0 1-1.685 2.197h.002Zm-6.01-5.636a3.438 3.438 0 1 0 0 6.876 3.438 3.438 0 0 0 0-6.876Zm0 5A1.562 1.562 0 1 1 10 8.438a1.562 1.562 0 0 1 0 3.124Z" />
+              </svg>
+              <h6 className="hidden md:block">Preview</h6>
             </Link>
           </div>
         </div>
--- src/app/dashboard/profile/page.tsx
+++ src/app/dashboard/profile/page.tsx
@@ -1,8 +1,71 @@
 export default function Profile() {
   return (
-    <div>
-      <h1>Profile Details</h1>
-      <p>Add your details to create a personal touch to your profile.</p>
+    <div className="p-4 pb-12">
+      <div className="flex flex-col relative bg-white rounded-md">
+        <div className="p-6">
+          <h1 className="text-2xl mb-3">Profile Details</h1>
+          <p className="text-gray-400">
+            Add your details to create a personal touch to your profile.
+          </p>
+
+          <div className="flex flex-col rounded-md bg-gray-50 mt-6 p-6">
+            <h6 className="mb-3">Profile picture</h6>
+            <div className="flex flex-col items-center justify-center rounded-md bg-purple-100 text-purple-600 p-8">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="40"
+                height="40"
+                fill="none"
+                viewBox="0 0 40 40"
+                className="fill-current mb-3"
+              >
+                <path d="M33.75 6.25H6.25a2.5 2.5 0 0 0-2.5 2.5v22.5a2.5 2.5 0 0 0 2.5 2.5h27.5a2.5 2.5 0 0 0 2.5-2.5V8.75a2.5 2.5 0 0 0-2.5-2.5Zm0 2.5v16.055l-4.073-4.072a2.5 2.5 0 0 0-3.536 0l-3.125 3.125-6.875-6.875a2.5 2.5 0 0 0-3.535 0L6.25 23.339V8.75h27.5ZM6.25 26.875l8.125-8.125 12.5 12.5H6.25v-4.375Zm27.5 4.375h-3.34l-5.624-5.625L27.91 22.5l5.839 5.84v2.91ZM22.5 15.625a1.875 1.875 0 1 1 3.75 0 1.875 1.875 0 0 1-3.75 0Z" />
+              </svg>
+              <p>+ Upload Image</p>
+            </div>
+
+            <small className="mt-6 text-gray-500">
+              Image must be below 1024x1024px. Use PNG or JPG format.
+            </small>
+          </div>
+
+          <div className="flex flex-col rounded-md bg-gray-50 mt-6 p-6">
+            <form>
+              <div className="mb-3">
+                <label className="block mb-2 text-sm font-medium">
+                  First Name*
+                </label>
+                <input
+                  type="text"
+                  className="bg-white border outline-none border-gray-300 text-gray-700 text-sm rounded-lg focus:border-purple-500 focus:ring-4 ring-purple-300 focus:shadow-md block w-full p-2.5"
+                />
+              </div>
+              <div className="mb-3">
+                <label className="block mb-2 text-sm font-medium">
+                  Last Name*
+                </label>
+                <input
+                  type="text"
+                  className="bg-white border outline-none border-gray-300 text-gray-700 text-sm rounded-lg focus:border-purple-500 focus:ring-4 ring-purple-300 focus:shadow-md block w-full p-2.5"
+                />
+              </div>
+              <div className="mb-3">
+                <label className="block mb-2 text-sm font-medium">Email*</label>
+                <input
+                  type="email"
+                  className="bg-white border outline-none border-gray-300 text-gray-700 text-sm rounded-lg focus:border-purple-500 focus:ring-4 ring-purple-300 focus:shadow-md block w-full p-2.5"
+                />
+              </div>
+            </form>
+          </div>
+        </div>
+
+        <div className="p-4 rounded-b-md border-t-2 border-gray-100">
+          <button className="w-full px-3 py-2 rounded-md font-bold bg-purple-300 text-white focus:ring-2">
+            Save
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
